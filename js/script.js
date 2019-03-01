c8ea224c0a8a67696723e692c2a0bc97cdd4d977
MonaMarchetti <marchetti.mona@gmail.com>
2019-03-01 15:41:15 +0100

    back to the accordion

--- js/script.js
+++ js/script.js
@@ -17,24 +17,23 @@ function myFunction() {
 
                 var resultat = "";
                 var name = "";
-                
-                // function show() {
-                // document.getElementById("open").style.display= "block";
-                // };
-                // show();
 
                 for (var i = 0; i <= nom.length - 1; i++) {
-                    if (nom[i] === '') { } else {
-                        resultat += "<div id='separ'><span class='sousTitre'><div id='open'>" + nom[i] + "</div></span><br>" + description[i] + "</div> <iframe src='" + liens[i] + "'></iframe>  ";
+                    if (nom[i] === '') {
+                    } else {
+                      resultat += "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='heading" + [i] +"'><h2 class='mb-0'><button class='btn btn-link' type='button' data-toggle='collapse' data-target='#collapse" + [i] +"' aria-expanded='false' aria-controls='collapse" + [i] +"'>"
+                      + nom[i] + "</button></h2></div><div id='collapse" + [i] +"' class='collapse' aria-labelledby='heading" + [i] +"' data-parent='#accordionExample'><div class='card-body'>"
+                      + description[i] + "<iframe src='"
+                      + liens[i] + "'></iframe></div></div></div></div>"
+
                         name += "<a href=" + liens[i] + ">" + nom[i] + "</a><br>";
 
                         document.getElementById("lien").innerHTML = resultat;
                         document.getElementById("menuGauche").innerHTML = name;
 
-                    }
+                      }
                 }
             });
         });
     }
 };
-// onclick="document.getElementById('demo').innerHTML = Date()
