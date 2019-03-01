90fd793c70b7e72aaa3c413b9fd2040615abf275
MonaMarchetti <marchetti.mona@gmail.com>
2019-03-01 16:00:54 +0100

    cor accordion

--- js/script.js
+++ js/script.js
@@ -21,10 +21,13 @@ function myFunction() {
                 for (var i = 0; i <= nom.length - 1; i++) {
                     if (nom[i] === '') {
                     } else {
-                      resultat += "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='heading" + [i] +"'><h2 class='mb-0'><button class='btn btn-link' type='button' data-toggle='collapse' data-target='#collapse" + [i] +"' aria-expanded='false' aria-controls='collapse" + [i] +"'>"
-                      + nom[i] + "</button></h2></div><div id='collapse" + [i] +"' class='collapse' aria-labelledby='heading" + [i] +"' data-parent='#accordionExample'><div class='card-body'>"
+                      resultat += "<div class='accordion' id='accordionExample'><div class='card'><div class='card-header' id='heading"
+                      + [i] +"'><h2 class='mb-0'><button class='btn btn-link' type='button' data-toggle='collapse' data-target='#collapse"
+                      + [i] +"' aria-expanded='false' aria-controls='collapse" + [i] +"'>"
+                      + nom[i] + "</button></h2></div><div id='collapse" + [i] +"' class='collapse' aria-labelledby='heading"
+                      + [i] +"' data-parent='#accordionExample'><div class='card-body'>"
                       + description[i] + "<iframe src='"
-                      + liens[i] + "'></iframe></div></div></div></div>"
+                      + liens[i] + "'></iframe></div></div></div></div>";
 
                         name += "<a href=" + liens[i] + ">" + nom[i] + "</a><br>";
 
