a929743dbb6a2f55c774247003813c9b8ff119d4
Djendo <HoBeOne@MacBook-Pro-de-Djendo-2.local>
2019-05-14 14:50:50 +0200

    Editing

---
+++ README.md
@@ -0,0 +1,12 @@
+#API Wikipedia
+
+## Project
+
+
+In this project, I use the wikipedia API to get the information from a word
+
+## Used Languages and technologies
+* Javascript
+* html, css3
+
+!['image'](/img/app-img.png)
--- js/script.js
+++ js/script.js
@@ -1,40 +1,45 @@
-function myFunction() {
-    var search = document.getElementById("search").value;
-    if (search === '') {
-        document.getElementById("titre").innerHTML = "Aucun resultat";
-        document.getElementById("lien").innerHTML = "";
-        document.getElementById("menuGauche").innerHTML = "";
+let title = document.getElementById("title");
+let link = document.getElementById("link");
+let menuGauche = document.getElementById("menuGauche");
+let search = document.getElementById("search");
+let myBtn = window.document.getElementById("myBtn")
+let linkIframe = window.document.getElementById("btnOpenFram")
+
+myBtn.addEventListener("click", ()=>{
+    if (!search.value) {
+        link.innerHTML = "";
+        menuGauche.innerHTML = "";
     }
     else {
-        URL = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=' + search + '';
-        fetch(URL).then(function (response) {
-            response.json().then(function (data) {
+        URL = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=' + search.value + '';
+        fetch(URL).then((response)=>{
+            response.json().then((data)=>{
+                console.log(data);
 
-                var titre = "<div id='separ'>" + data[0] + "</div>";
-                var nom = data[1];
-                var description = data[2];
-                var liens = data[3];
-
-                var resultat = "";
-                var name = "";
-                
-                // function show() {
-                // document.getElementById("open").style.display= "block";
-                // };
-                // show();
+                let title = "<div id='separ'>" + data[0] + "</div>";
+                let nom = data[1];
+                let description = data[2];
+                let liens = data[3];
+                let resultat = "";
+                let name = "";
+                let linkIframe;
+                let frame;
 
                 for (var i = 0; i <= nom.length - 1; i++) {
-                    if (nom[i] === '') { } else {
-                        resultat += "<div id='separ'><span class='sousTitre'><div id='open'>" + nom[i] + "</div></span><br>" + description[i] + "</div> <iframe src='" + liens[i] + "'></iframe>  ";
-                        name += "<a href=" + liens[i] + ">" + nom[i] + "</a><br>";
 
-                        document.getElementById("lien").innerHTML = resultat;
-                        document.getElementById("menuGauche").innerHTML = name;
+                    frame = "<iframe src='" + liens[i] + "'></iframe>"; 
+                    linkIframe = "<br><br><button type='button' id=" + i + "  >Voir le site " + i + "</button>";
 
-                    }
+                    resultat += "<div id='separ'><span class='sousTitre'><div id='open'>" + nom[i] + "</div></span><br>" + description[i] + linkIframe + "</div>" + frame;
+                    name += "<a href=" + liens[i] + ">" + nom[i] + "</a><br>";
+
                 }
+                title.innerHTML = '';
+                link.innerHTML = resultat;
+                menuGauche.innerHTML = name;
+               
             });
         });
     }
-};
-// onclick="document.getElementById('demo').innerHTML = Date()
+});
+
